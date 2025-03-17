01e266b08015318072066cb84e1227fd80450e04
Max Knerrich <max.knerrich@outlook.com>
2025-03-17 17:59:24 +0100

    added common packages

---
+++ hosts/common/common-packages.nix
@@ -0,0 +1,27 @@
+{ inputs, pkgs, unstablePkgs, ... }:
+let
+  inherit (inputs) nixpkgs nixpkgs-unstable;
+in
+{
+  nixpkgs.config.allowUnfree = true;
+  environment.systemPackages = with pkgs; [
+    ## stable
+    btop
+    coreutils
+    drill
+    fastfetch
+    fd
+    figurine
+    gh
+    git-crypt
+    iperf3
+    jq
+    mc
+    ripgrep
+    tree
+    unzip
+    watch
+    wget
+    zoxide
+  ];
+}
