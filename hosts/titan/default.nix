f48205f0bd9b9045ab46fadcf64328e543165366
Max Knerrich <max.knerrich@outlook.com>
2025-03-18 01:18:49 +0100

    fix

--- hosts/titan/default.nix
+++ hosts/titan/default.nix
@@ -49,6 +49,7 @@
   users.users.mkn = {
     isNormalUser = true;
     extraGroups = ["wheel" "docker" "render" "video"];
+    hashedPassword = "$6$8vCdBRvC7OxmyZ/I$5GVwEMJVtJ87I4GJnb2xkadsAovhaB2a0.jY2hto.yfr8JcEopvNwCQi0Nnn3lgzPUx4i9MdvpguRcbgR99JG0";
     openssh.authorizedKeys.keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN2tkxTzD2+lfM6QCxJwJFchIggPdzcZhQJjFTaRZvKg max.knerrich@outlook.com"];
     packages = with pkgs; [
       home-manager
