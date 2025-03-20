378cb75e02a2f339f6be7bdf72043a97f69fa4a5
Max Knerrich <max.knerrich@outlook.com>
2025-03-20 12:03:03 +0100

    added snapraid sync and scrub

--- hosts/titan/snapraid.nix
+++ hosts/titan/snapraid.nix
@@ -1,4 +1,8 @@
-{...}: {
+{
+  config,
+  lib,
+  ...
+}: {
   services.snapraid = {
     enable = true;
     parityFiles = ["/mnt/parity1/snapraid.parity"];
@@ -18,5 +22,47 @@
       "appdata/"
       "*.!sync"
     ];
+  };
+  systemd = {
+    timers = {
+      snapraid-sync = {
+        wantedBy = ["timers.target"];
+        timerConfig = {
+          # Run daily at 2:00 AM
+          OnCalendar = "13:00:00";
+          # If system was off when timer should have run, run it soon after boot
+          Persistent = true;
+        };
+      };
+
+      snapraid-scrub = {
+        wantedBy = ["timers.target"];
+        timerConfig = {
+          # Run weekly on Sundays at 23:00
+          OnCalendar = "Sun 23:00:00";
+          Persistent = true;
+        };
+      };
+    };
+    services = lib.attrsets.optionalAttrs (config.services.snapraid.enable) {
+      snapraid-sync = {
+        onFailure = lib.lists.optionals (config ? tg-notify && config.tg-notify.enable) [
+          "tg-notify@%i.service"
+        ];
+        serviceConfig = {
+          RestrictNamespaces = lib.mkForce false;
+          RestrictAddressFamilies = lib.mkForce "";
+        };
+      };
+      snapraid-scrub = {
+        onFailure = lib.lists.optionals (config ? tg-notify && config.tg-notify.enable) [
+          "tg-notify@%i.service"
+        ];
+        serviceConfig = {
+          RestrictNamespaces = lib.mkForce false;
+          RestrictAddressFamilies = lib.mkForce "";
+        };
+      };
+    };
   };
 }
