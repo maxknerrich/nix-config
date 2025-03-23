40e0e044bfac7333551321404a9070b0ad7caae2
Max Knerrich <max.knerrich@outlook.com>
2025-03-23 21:27:51 +0100

    fix missing id

--- nixos/hardware-configuration.nix
+++ nixos/hardware-configuration.nix
@@ -13,7 +13,7 @@
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
-  boot.initrd.availableKernelModules = ["xhci_pci" "ehci_pci" "ahci" "usbhid" "uas" "sd_mod"];
+  # boot.initrd.availableKernelModules = ["xhci_pci" "ehci_pci" "ahci" "usbhid" "uas" "sd_mod"];
   boot.initrd.kernelModules = [];
   boot.kernelModules = ["kvm-intel"];
   boot.extraModulePackages = [];
@@ -32,6 +32,24 @@
   # };
   # # Optimize BTRFS settings
   # boot.supportedFilesystems = ["btrfs"];
+
+  zfs-root = {
+    boot = {
+      devNodes = "/dev/disk/by-id/";
+      bootDevices = ["ata-SanDisk_SSD_PLUS_240GB_24313W802439" "ata-INTEL_SSDSC2BA200G3_BTTV5335017Q200GGN"];
+      immutable = false;
+      availableKernelModules = [
+        "xhci_pci"
+        "ehci_pci"
+        "ahci"
+        "usbhid"
+        "uas"
+        "sd_mod"
+      ];
+      removableEfi = true;
+    };
+  };
+
   boot.kernelParams = ["noatime"];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
