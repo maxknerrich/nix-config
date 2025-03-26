1afdd15901933d062a664cb33baf9378f1e6ec20
Max Knerrich <max.knerrich@outlook.com>
2025-03-26 15:07:30 +0100

    fix git email

--- home/home.nix
+++ home/home.nix
@@ -75,7 +75,7 @@
   programs.git = {
     enable = true;
     userName = "Max Knerrich";
-    userEmail = "max.knerrich@outook.com";
+    userEmail = "max.knerrich@outlook.com";
     extraConfig = {
       init.defaultBranch = "main";
     };
