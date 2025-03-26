55f48e972c79c24e03771f0ba50cf76a8bb526c2
Max Knerrich <max.knerrich@outlook.com>
2025-03-26 22:56:12 +0100

    added hd-idle

--- nixos/filesystems/default.nix
+++ nixos/filesystems/default.nix
@@ -30,4 +30,17 @@
     ];
     fsType = "fuse.mergerfs";
   };
+  systemd.services.hd-idle = {
+    description = "HDD spin down daemon";
+    wantedBy = ["multi-user.target"];
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = ''
+        ${pkgs.hd-idle}/bin/hd-idle -i 0 \
+            -a /dev/disk/by-id/ata-TOSHIBA_DT01ACA200_Z32UXMXGS -i 600 \
+            -a /dev/disk/by-id/ata-SAMSUNG_HD103SI_S1VSJDWZ570410 -i 600 \
+            -a /dev/disk/by-id/ata-ST1000LM014-1EJ164_W7708ZYV   -i 600
+      '';
+    };
+  };
 }
