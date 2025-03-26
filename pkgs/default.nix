3de8062aa66dee6675947955da31aa52f425f4dd
Max Knerrich <max.knerrich@outlook.com>
2025-03-26 15:08:20 +0100

    remove unused package

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -6,5 +6,4 @@
   cockpit-sensors = pkgs.callPackage ./cockpit/sensors {};
 
   snapraid-btrfs = pkgs.callPackage ./snapraid-btrfs {};
-  snapraid-btrfs-runner = pkgs.callPackage ./snapraid-btrfs/runner.nix {};
 }
